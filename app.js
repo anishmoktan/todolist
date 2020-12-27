1c883b6c21acadc97205ff0f6c6e4c34aa582efc
anishmoktan <anish.tmngo@gmail.com>
2020-12-26 19:57:20 -0500

    270

--- app.js
+++ app.js
@@ -7,32 +7,32 @@ const ejs = require("ejs"); // include ejs module
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs'); //enables you to use static template files in your application using ejs
 
 app.use(bodyParser.urlencoded({extended: true})); //using it to be able to read the incoming json from frontend
-app.use(express.static("public")); //enables use of built-in middleware for serving static files
+app.use(express.static("public")); //enables use of built-in middleware for serving files in puclic folder on the internet (css files)
 
 
 app.get("/", function(req, res){ //data server gets from home "/" page
 
-  var today = new Date(); //declares date fucntion in JS
+  let today = new Date(); //declares date fucntion in JS
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleString("en-US",options);
+  let day = today.toLocaleString("en-US",options);
 
   res.render("list", {kindOfDay: day, newListItems: items}); //links the list.ejs file to kindOfDay variable in the html file to show in the screen
     //replaces kindOfDay in the list.ejs file with day variable
 });
 
 app.post("/",function(req,res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 
