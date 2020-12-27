48467ae46e4a7be37ea30d9271f0d9bb8d09e15f
anishmoktan <anish.tmngo@gmail.com>
2020-12-26 19:29:12 -0500

    before changing to loop in ejs file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('view engine', 'ejs'); //enables you to use static template files in your application using ejs
 
-app.use(bodyParser.urlencoded({extended: true})); //parsing the properties in the body
+app.use(bodyParser.urlencoded({extended: true})); //using it to be able to read the incoming json from frontend
 app.use(express.static("public")); //enables use of built-in middleware for serving static files
 
 
@@ -24,12 +24,15 @@ app.get("/", function(req, res){ //data server gets from home "/" page
   };
 
   var day = today.toLocaleString("en-US",options);
-  
-
 
   res.render("list", {kindOfDay: day}); //links the list.ejs file to kindOfDay variable in the html file to show in the screen
     //replaces kindOfDay in the list.ejs file with day variable
 });
+
+app.post("/",function(req,res){
+  var item = req.body.newItem;
+  console.log(item);
+})
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
